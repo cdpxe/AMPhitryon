3ba8785850e3fb4a140d1ac7d9b52f0b293a8dec
Steffen Wendzel <steffen.wendzel@uni-ulm.de>
2026-02-17 14:20:10 +0100

    runtime evaluation now also done for worst-case setting

--- runtime_eval.sh
+++ runtime_eval.sh
@@ -10,13 +10,21 @@ export CLANG=$LANG
 CHUNK_SIZE_LIST="4 5 6 7 8 9 10 12 15 20 25 30 35 40 45 50"
 DICT_SIZE_LIST="10000 50000 100000 200000 500000 1000000 2000000 4000000 " #5000000 8388606" #max
 aDICT=empty.dict_a # for incr
+A_FILE=A #temporary file containing only "A" characters
 MSG_FILE=msg.file
 MESSAGE_SIZES="4 8 10 12 14 18 22 26 30 34 38 42 50"
+
+perl -e 'print "A"x40000000' > $A_FILE;
+chmod +t amphi
 
 echo "MessageSize;DictFileSize;ChunkSize;ComputationTime"
 for MESSAGE_SIZE in $MESSAGE_SIZES ; do
     #echo "MESSAGE_SIZE=$MESSAGE_SIZE"
-    dd if=/dev/zero of=$MSG_FILE count=$MESSAGE_SIZE 2>/dev/null
+
+    # The following dd if=$A_FILE is the worst-case setting, as we search for "AAAA..."
+    # but such a string cannot be found in the dictionary, at all. For the best-case
+    # setting, we need to search for a string composed with if=/dev/zero.
+    dd if=$A_FILE of=$MSG_FILE count=$MESSAGE_SIZE 2>/dev/null
     for DICTSIZE in $DICT_SIZE_LIST ; do
         #echo "   DICTSIZE=$DICTSIZE"
         dd if=/dev/zero of=$aDICT count=$DICTSIZE 2>/dev/null
